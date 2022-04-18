5104460cb88b8a7978630c7dc9d373c237786c25
niko89081 <90409933+niko89081@users.noreply.github.com>
2022-04-18 12:35:19 -0400

    Update facial.py

--- facial.py
+++ facial.py
@@ -2,22 +2,39 @@ import cv2 as cv
 from deepface import DeepFace
 cascPath = "something.xml"
 faceCascade = cv.CascadeClassifier(cascPath)
-image = cv.imread("Screenshot 2021-02-07 023640.png")
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags= cv.CASCADE_SCALE_IMAGE
-)
+vid = cv.VideoCapture(0)
+while (True):
+
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+
+    # Display the resulting frame
+    
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv.CASCADE_SCALE_IMAGE
+    )
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if len(faces):
+        for (x, y, w, h) in faces:
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        break
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    cv.imshow('frame', frame)
+# After the loop release the cap object
+# Destroy all the windows
+cv.destroyAllWindows()
+
 print(faces)
 
 # Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-obj = DeepFace.analyze(img_path = "Screenshot 2021-02-07 023640.png", actions= ['age', 'gender', 'race', 'emotion'])
-print(obj)
-cv.imshow("Faces found", image)
 cv.waitKey(0)
 cv.destroyAllWindows()
